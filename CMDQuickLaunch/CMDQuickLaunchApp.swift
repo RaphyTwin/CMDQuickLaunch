f11de77d0a8488500fb859b888ec726b40916f9d
RaphyTwin <raphy-twin@outlook.com>
2025-01-08 16:07:17 +0100

    Initial Commit

---
+++ CMDQuickLaunch/CMDQuickLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMDQuickLaunchApp.swift
+//  CMDQuickLaunch
+//
+//  Created by Raphy on 08.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CMDQuickLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
